3cdc0b3c470bb642c52a33301b3890ce6c6311b1
khushiSaxena09 <140104776+khushiSaxena09@users.noreply.github.com>
2026-05-22 10:51:02 +0530

    commit

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL ||
-  "https://skribbl-sm6v.onrender.com";
+  // "https://skribbl-sm6v.onrender.com";
+  "https://skribbl-1.onrender.com";
 
 export const socket = io(SOCKET_URL);
